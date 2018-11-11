f68700daa5a61e7d2ff26d43cc3bf8b628bf524a
JanAbe <jandewaal33@gmail.com>
2018-11-11 19:09:30 +0100

    Fix bug and add comments

--- course3/week4/module4.go
+++ course3/week4/module4.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
+	// make chopsticks
 	chopsticks := make([]*ChopStick, 5)
 	for i := 0; i < 5; i++ {
 		chopsticks[i] = &ChopStick{id: i}
 	}
 
+	// make philosophers
 	philosophers := make([]*Philosopher, 5)
 	for i := 0; i < 5; i++ {
 		philosophers[i] = &Philosopher{i, chopsticks[i], chopsticks[(i+1)%5], 3}
@@ -20,6 +22,9 @@ func main() {
 	host := &Host{c: make(chan int, 2)}
 	wg.Add(15)
 
+	// give permission to a philosopher -> pass it into a channel
+	// read from the channel -> this philosopher can eat
+	// philosophers[x] works because the id and index correspond (this is kinda sketchy though)
 	for i := 0; i < len(philosophers); i++ {
 		go host.givePermission(philosophers[i].id)
 		x := <-host.c
@@ -48,7 +53,11 @@ type Philosopher struct {
 }
 
 func (p Philosopher) eat(wg *sync.WaitGroup) {
-	for p.hungry > 0 {
+	// why does it sometimes happen that a philosopher eats more than 3 times?
+	// the p.hungry is inside a lock() right?
+	// changing it to a normal forloop fixes it
+	// for p.hungry > 0 {
+	for i := 0; i < 3; i++ {
 		p.leftCS.Lock()
 		p.rightCS.Lock()
 
